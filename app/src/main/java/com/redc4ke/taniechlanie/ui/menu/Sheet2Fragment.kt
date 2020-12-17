62afb0ab55d68a5c888ac8e9b11c8551c4473d06
Michał Wyczarski <64838595+RedC4ke@users.noreply.github.com>
2020-12-17 18:23:43 +0100

    Details page rework completed

--- app/src/main/java/com/redc4ke/taniechlanie/ui/menu/Sheet2Fragment.kt
+++ app/src/main/java/com/redc4ke/taniechlanie/ui/menu/Sheet2Fragment.kt
@@ -1,17 +1,24 @@
 package com.redc4ke.taniechlanie.ui.menu
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.SearchView
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.redc4ke.taniechlanie.R
+import com.redc4ke.taniechlanie.data.AlcoObject
+import com.redc4ke.taniechlanie.data.Shop
 import com.redc4ke.taniechlanie.data.ShopViewModel
 import com.redc4ke.taniechlanie.data.menu.DetailsShopAdapter
 import com.redc4ke.taniechlanie.ui.BaseFragment
 import kotlinx.android.synthetic.main.fragment_sheet2.*
+import java.util.*
 
 class Sheet2Fragment : BaseFragment() {
 
@@ -48,11 +55,51 @@ class Sheet2Fragment : BaseFragment() {
             val adapter = DetailsShopAdapter(detailsFragment.alcoObject, it)
             recyclerView.adapter = adapter
             adapter.notifyDataSetChanged()
+            setupSearchBar(sheet2_ET, detailsFragment.alcoObject, it, adapter)
         })
 
 
         sheet2_returnBT.setOnClickListener {
             findNavController().navigate(R.id.action_sheet2_dest_to_sheet1_dest)
         }
+    }
+
+    private fun setupSearchBar(
+            editText: EditText,
+            alcoObject: AlcoObject,
+            shopList: Map<Int, Shop>,
+            adapter: DetailsShopAdapter) {
+        editText.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val filtered = arrayListOf<Int>()
+                val available = mutableMapOf<Int, Shop>()
+                alcoObject.shop.forEach {
+                    if (shopList[it] != null) {
+                        available[it] = shopList[it] as Shop
+                    }
+                }
+
+                if (s.toString() != "") {
+                    for (item in available) {
+                        if (item.value.name.toLowerCase(Locale.ROOT)
+                                        .contains(s.toString().toLowerCase(Locale.ROOT))) {
+                            filtered.add(item.key)
+                        }
+                    }
+                    adapter.update(filtered)
+                } else {
+                    adapter.update(alcoObject.shop)
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
     }
 }
