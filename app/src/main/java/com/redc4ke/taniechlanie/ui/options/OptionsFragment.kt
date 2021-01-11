3fb8eb16d9cd4cfb0cb0462f4d0c89494a35a0d3
Michał Wyczarski <64838595+RedC4ke@users.noreply.github.com>
2021-01-11 17:50:17 +0100

    v1.0.1
    Added option to change value unit

--- app/src/main/java/com/redc4ke/taniechlanie/ui/options/OptionsFragment.kt
+++ app/src/main/java/com/redc4ke/taniechlanie/ui/options/OptionsFragment.kt
@@ -1,12 +1,17 @@
 package com.redc4ke.taniechlanie.ui.options
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.redc4ke.taniechlanie.R
+import kotlinx.android.synthetic.main.fragment_options.*
+import kotlin.math.round
 
 class OptionsFragment : Fragment() {
 
@@ -21,5 +26,40 @@ class OptionsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_options, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        optionPickUnit()
+    }
+
+    private fun optionPickUnit() {
+        val spinner = options_unit_SPINNER
+        val list = arrayListOf("R", "mR")
+        val adapter = ArrayAdapter(
+            requireContext(), android.R.layout.simple_spinner_dropdown_item, list)
+        spinner.adapter = adapter
+        val prefs = requireActivity().getPreferences(Context.MODE_PRIVATE)
+        val rounded = prefs.getBoolean("rounded_mR", false)
+        val selection = if (rounded) 0 else 1
+        spinner.setSelection(selection)
+
+        spinner.onItemSelectedListener =  object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selected = position == 0
+                prefs.edit().putBoolean("rounded_mR", selected).apply()
+                Log.d("huj", selected.toString())
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+        }
     }
 }
