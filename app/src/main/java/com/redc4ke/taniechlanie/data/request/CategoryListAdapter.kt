5191e4307a9761b26e940d5b064a3b835ebbe13c
Michał Wyczarski <64838595+RedC4ke@users.noreply.github.com>
2021-01-31 00:05:59 +0100

    v2.2.1

--- app/src/main/java/com/redc4ke/taniechlanie/data/request/CategoryListAdapter.kt
+++ app/src/main/java/com/redc4ke/taniechlanie/data/request/CategoryListAdapter.kt
@@ -1,7 +1,6 @@
 package com.redc4ke.taniechlanie.data.request
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.redc4ke.taniechlanie.data.Category
--- app/src/main/java/com/redc4ke/taniechlanie/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/redc4ke/taniechlanie/ui/profile/ProfileFragment.kt
@@ -1,14 +1,34 @@
 package com.redc4ke.taniechlanie.ui.profile
 
+import android.content.Context
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseUser
+import com.redc4ke.taniechlanie.R
 import com.redc4ke.taniechlanie.databinding.FragmentProfileBinding
 import com.redc4ke.taniechlanie.ui.BaseFragment
+import com.redc4ke.taniechlanie.ui.MainActivity
+import com.squareup.picasso.Picasso
 
 class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
 
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentProfileBinding
         get() = FragmentProfileBinding::inflate
+    private lateinit var user: FirebaseUser
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        user = (requireActivity() as MainActivity).auth.currentUser!!
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with (binding) {
+            profilePictureIV.setImageResource(R.drawable.avatar)
+            profileNameTV.text = user.displayName
+        }
+    }
 }
