33d531a2e0d78a55854d1d2e916cfff852ca599e
Michał Wyczarski <64838595+RedC4ke@users.noreply.github.com>
2021-03-22 19:45:45 +0100

    Revert "Revert "v0.5.1 - now working""
    This reverts commit beaea9f71c0c432fd4e8077ddb70b9ccf16bf4d8.

--- app/src/main/java/com/redc4ke/taniechlanie/ui/menu/MenuFragment.kt
+++ app/src/main/java/com/redc4ke/taniechlanie/ui/menu/MenuFragment.kt
@@ -1,7 +1,34 @@
 package com.redc4ke.taniechlanie.ui.menu
 
-import com.redc4ke.taniechlanie.ui.base.BaseFragment
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModelProvider
+import com.redc4ke.taniechlanie.data.AlcoObject
+import com.redc4ke.taniechlanie.data.viewmodels.AlcoObjectViewModel
+import com.redc4ke.taniechlanie.databinding.FragmentMenuBinding
+import com.redc4ke.taniechlanie.ui.base.BaseListFragment
 
-class MenuFragment() {
+class MenuFragment() : BaseListFragment<FragmentMenuBinding>() {
+
+    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentMenuBinding
+        get() = FragmentMenuBinding::inflate
+    override val alcoObjectList = MutableLiveData<List<AlcoObject>>()
+    private lateinit var alcoObjectViewModel: AlcoObjectViewModel
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        alcoObjectViewModel = ViewModelProvider(requireActivity())[AlcoObjectViewModel::class.java]
+    }
+
+    override fun onStart() {
+        super.onStart()
+        alcoObjectViewModel.getAll().observe(viewLifecycleOwner, {list ->
+            alcoObjectList.value = list.sortedBy { it.name }
+        })
+    }
 
 }
--- app/src/main/java/com/redc4ke/taniechlanie/ui/menu/details/DetailsFragment.kt
+++ app/src/main/java/com/redc4ke/taniechlanie/ui/menu/details/DetailsFragment.kt
@@ -14,6 +14,7 @@ import com.redc4ke.taniechlanie.data.viewmodels.UserViewModel
 import com.redc4ke.taniechlanie.databinding.FragmentDetailsBinding
 import com.redc4ke.taniechlanie.ui.base.BaseFragment
 import com.redc4ke.taniechlanie.ui.MainActivity
+import com.redc4ke.taniechlanie.ui.menu.AlcoListFragment
 import com.redc4ke.taniechlanie.ui.menu.MenuFragment
 
 class DetailsFragment : BaseFragment<FragmentDetailsBinding>() {
@@ -21,7 +22,7 @@ class DetailsFragment : BaseFragment<FragmentDetailsBinding>() {
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentDetailsBinding
         get() = FragmentDetailsBinding::inflate
     private lateinit var mainActivity: MainActivity
-    private lateinit var parentFrag: MenuFragment
+    private lateinit var parentFrag: AlcoListFragment
     lateinit var alcoObject: AlcoObject
     lateinit var userViewModel: UserViewModel
 
@@ -35,7 +36,7 @@ class DetailsFragment : BaseFragment<FragmentDetailsBinding>() {
         super.onCreate(savedInstanceState)
 
         mainActivity = requireActivity() as MainActivity
-        parentFrag = arguments?.getSerializable("MenuFragment") as MenuFragment
+        parentFrag = arguments?.getSerializable("AlcoListFragment") as AlcoListFragment
         alcoObject = arguments?.getSerializable("alcoObject") as AlcoObject
 
         sharedElementEnterTransition = MaterialContainerTransform().apply {
@@ -48,6 +49,7 @@ class DetailsFragment : BaseFragment<FragmentDetailsBinding>() {
             duration = 200
         }
 
+        (requireActivity() as MainActivity).supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
