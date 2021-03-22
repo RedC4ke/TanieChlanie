697909e5b0f1b96d98050881dc2b5c4394aa8482
Michał Wyczarski <64838595+RedC4ke@users.noreply.github.com>
2021-03-22 19:45:35 +0100

    Revert "v0.5.1 - now working"
    This reverts commit 38cdc380cdfeefef0bea8e3826d2003b7bc5e5d3.

--- app/src/main/java/com/redc4ke/taniechlanie/ui/base/BaseListFragment.kt
+++
@@ -1,9 +0,0 @@
-package com.redc4ke.taniechlanie.ui.base
-
-import androidx.lifecycle.MutableLiveData
-import androidx.viewbinding.ViewBinding
-import com.redc4ke.taniechlanie.data.AlcoObject
-
-abstract class BaseListFragment<VB: ViewBinding> : BaseFragment<VB>() {
-    abstract val alcoObjectList: MutableLiveData<List<AlcoObject>>
-}
